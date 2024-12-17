658c25dcc007256711ff46f4004c801556bc1123
Fernando Blasco <fernando@sumun.net>
2024-12-17 10:12:32 +0100

    Search results

--- loop-templates/content-search.php
+++ loop-templates/content-search.php
@@ -9,39 +9,24 @@
 defined( 'ABSPATH' ) || exit;
 ?>
 
-<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+<article class="stretch-linked-block position-relative" id="post-<?php the_ID(); ?>">
 
-	<header class="entry-header">
+	<header class="entry-header d-flex align-items-center justify-content-between gap-3 py-2 border-bottom">
 
 		<?php
 		the_title(
-			sprintf( '<h2 class="entry-title"><a class="stretched-link" href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
-			'</a></h2>'
+			sprintf( '<p class="entry-title mb-0"><a class="stretched-link" href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
+			'</a></p>'
 		);
 		?>
 
-		<?php if ( 'post' === get_post_type() ) : ?>
-
-			<div class="entry-meta">
-
-				<?php // understrap_posted_on(); ?>
-
-			</div><!-- .entry-meta -->
-
-		<?php endif; ?>
+		<?php
+		$post_type = get_post_type_object( get_post_type() );
+		if ( $post_type ) {
+			echo '<div class="post-type-name small text-muted">' . esc_html( $post_type->labels->singular_name ) . '</div>';
+		}
+		?>
 
 	</header><!-- .entry-header -->
-
-	<div class="entry-summary">
-
-		<?php the_excerpt(); ?>
-
-	</div><!-- .entry-summary -->
-
-	<footer class="entry-footer">
-
-		<?php // understrap_entry_footer(); ?>
-
-	</footer><!-- .entry-footer -->
 
 </article><!-- #post-## -->
